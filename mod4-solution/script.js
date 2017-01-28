d0521f9eda2c1d27ade96965a393da332528b655
Held Grijo <shadyueh@gmail.com>
2017-01-28 11:49:01 -0400

    Step 1: wrapping inside an IIFE

--- mod4-solution/script.js
+++ mod4-solution/script.js
@@ -34,6 +34,8 @@ WARNING!!! WARNING!!!
 // See Lecture 52, part 2
 // (Note, Step 2 will be done in the SpeakHello.js file.)
 
+(function(){
+  
 var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 
 // STEP 10:
@@ -63,3 +65,5 @@ for (/* fill in parts of the 'for' loop to loop over names array */) {
     // helloSpeaker.xxxx
   }
 }
+
+})();
