3b2675f296afecdc25c6a1042a4fe7d4c21c1061
Held Grijo <shadyueh@gmail.com>
2017-01-28 12:02:47 -0400

    Step 5: Exposing the helloSpeaker object to global context

--- mod4-solution/SpeakHello.js
+++ mod4-solution/SpeakHello.js
@@ -1,6 +1,6 @@
 // STEP 2: Wrap the entire contents of SpeakHello.js inside of an IIFE
 // See Lecture 52, part 2
-(function () {
+(function (window) {
 
 // STEP 3: Create an object, called 'helloSpeaker' to which you will attach
 // the "speak" method and which you will expose to the global context
@@ -21,6 +21,6 @@ helloSpeaker.speak = function (name) {
 // 'helloSpeaker' on the global scope as well.
 // See Lecture 52, part 2
 // (Note, Step 6 will be done in the SpeakGoodBye.js file.)
-// xxxx.xxxx = helloSpeaker;
+window.helloSpeaker = helloSpeaker;
 
-})();
+})(window);
