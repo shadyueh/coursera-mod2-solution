7ad27e94dd0b18f1aae0164f60d30a1a5378cb54
Held Grijo <shadyueh@gmail.com>
2017-01-28 12:13:37 -0400

    Step 9: Exposing the byeSpeaker object to global context

--- mod4-solution/SpeakGoodBye.js
+++ mod4-solution/SpeakGoodBye.js
@@ -3,7 +3,7 @@
 
 // STEP 6: Wrap the entire contents of SpeakGoodBye.js inside of an IIFE
 // See Lecture 52, part 2
-(function () {
+(function (window) {
 
 // STEP 7: Create an object, called 'byeSpeaker' to which you will attach
 // the "speak" method and which you will expose to the global context
@@ -22,6 +22,6 @@ byeSpeaker.speak = function (name) {
 
 // STEP 9: Expose the 'byeSpeaker' object to the global scope. Name it
 // 'byeSpeaker' on the global scope as well.
-// xxxx.xxxx = byeSpeaker;
+window.byeSpeaker = byeSpeaker;
 
-})();
+})(window);
